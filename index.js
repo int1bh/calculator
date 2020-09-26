22c55d336c0433ef2c578df536618d71bb8e1de0
ALEXANDR <alexandrdryagin@yandex.ru>
2020-09-27 02:23:56 +0300

    85% complite

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /*                                      Управление калькулятором
 *******************************************************************************************
 */
-const display = document.getElementsByTagName('input'); //дисплей
+let display = document.querySelector('#display'); //дисплей
 const digitsButtons = document.querySelectorAll('[digits]'); //цифровые кнопки
 const operationButtons = document.querySelectorAll('[operators]'); //кнопки операций
-const clearButton = document.getElementById('clear'); //сброс значения
-const ClearAllButton = document.getElementById('clear-all');
-const decimalButton = document.getElementById('decimal');
-const resultButton = document.getElementById('result');
+const clearButton = document.querySelector('#clear-all'); //полный сброс
+const decimalButton = document.querySelector('#decimal');//десятичная точка
+const deleteButton = document.querySelector('[delete]');
 
 //******************************************************************************************
 
@@ -17,17 +16,105 @@ const resultButton = document.getElementById('result');
 
 let currentValue = 0,
     newValue = false,
-    currentOperation = '',
+    //currentOperation = '',
     sqrSqrtNumber = false,
     numberToPow = 0;
+    
+    
+    let MemoryPendingOperation = '';    
 
 //*******************************************************************************************
+
+
+function pressNumber() {
+digitsButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (newValue) {
+            display.placeholder = e.target.textContent;
+            newValue = false;
+        } else {
+            if (display.placeholder === '0') {
+                display.placeholder = e.target.textContent;
+            } else {
+                display.placeholder += e.target.textContent;
+            }
+        }
+        return;
+    });
+} );
+}
+
+pressNumber();
+
+function getOperation() {
+    operationButtons.forEach(operationButton => {
+        operationButton.addEventListener('click', (e) => {
+            let localOperationMemory = Number(display.placeholder);
+  
+    if (newValue && MemoryPendingOperation !== '=') {
+      display.placeholder = currentValue;
+    } else {
+        newValue = true;
+      if (MemoryPendingOperation === '+') {
+        currentValue += localOperationMemory;
+      } else if (MemoryPendingOperation === '-') {
+        currentValue -= localOperationMemory;
+      } else if (MemoryPendingOperation === '×') {
+        currentValue *= localOperationMemory;
+      } else if (MemoryPendingOperation === '÷') {
+        currentValue /= localOperationMemory;
+      } else if (MemoryPendingOperation === 'xn') {
+          currentValue = Math.pow(currentValue, localOperationMemory);
+      }
+        else {
+        currentValue = localOperationMemory;
+      }
+      display.placeholder = currentValue;
+      MemoryPendingOperation = e.target.textContent;
+    }
+        });
+    });
+}
+
+getOperation();
+
+function clearAll() {
+    clearButton.addEventListener('click', (e) => {
+        if (e.target.textContent === 'C') {
+            display.placeholder = '0';
+            newValue = true;
+            currentValue = 0;
+            MemoryPendingOperation = '';
+        }
+    });
+}
+
+clearAll();
+
+function deleteValue() {
+    deleteButton.addEventListener('click', (e) => { 
+        if (display.placeholder === '0') {
+            return;
+        }
+        
+        let lengthValue = display.placeholder.length;
+        if (lengthValue === 1) {
+            display.placeholder = '0';
+        } else {
+            display.placeholder = display.placeholder.substring(0, lengthValue-1);
+        }
+    });
+
+}
+
+deleteValue();
+
+
 
 console.log(display);
 console.log(digitsButtons);
 console.log(operationButtons);
 console.log(clearButton);
-console.log(ClearAllButton);
 console.log(decimalButton);
-console.log(resultButton);
+
  
